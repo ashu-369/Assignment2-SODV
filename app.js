b7d6486bc0b74c423af4b01ac2f94eed7f935054
ashu-369 <145891277+ashu-369@users.noreply.github.com>
2024-04-08 23:00:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function confirmation() {
   const data = new URLSearchParams(formData).toString();
 
   // Send POST request
-  fetch("https://assignment2-backend-k9qg.onrender.com/", {
+  fetch("https://assignment2-backend-k9qg.onrender.com/register", {
     method: "POST",
     body: data,
     headers: {
